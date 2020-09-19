aa8f897d51737c05b35ddb838113c6c1780bdaf3
Martyna Gorna <mart.gorna@gmail.com>
2020-09-19 17:40:57 +0100

    Home page with some css styles

---
+++ portfolio/src/components/Home.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "../styles/Home.css";
+
+const Home = () => {
+  return (
+    <div className="homePage">
+      <div className="intro">
+        <div className="hello">
+          <span class="light">H</span>
+          <span className="lighter">E_</span>
+          <br></br>
+          <span>LLO</span>
+        </div>
+        <div className="about">
+          Hi, I'm Martyna. Lorem ipsum dolor sit amet, consectetur adipiscing
+          elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+          aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+          laboris nisi ut aliquip ex ea commodo consequat.
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;

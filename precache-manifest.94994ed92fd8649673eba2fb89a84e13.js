da910e1808a56a56e7704fe22c772bd94a8d15ea
Martyna Gorna <mart.gorna@gmail.com>
2020-09-21 13:12:19 +0100

    Updates

---
+++ precache-manifest.94994ed92fd8649673eba2fb89a84e13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5de4ca5bc96fe8f70259969bc05c6aab",
+    "url": "/portfolio-website/index.html"
+  },
+  {
+    "revision": "537b7ba8a17217a4bc85",
+    "url": "/portfolio-website/static/css/main.64f90f0e.chunk.css"
+  },
+  {
+    "revision": "1f75cc05bf4d4420d51e",
+    "url": "/portfolio-website/static/js/2.7c3dc78c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-website/static/js/2.7c3dc78c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "537b7ba8a17217a4bc85",
+    "url": "/portfolio-website/static/js/main.f505f600.chunk.js"
+  },
+  {
+    "revision": "a77bb18274229563e654",
+    "url": "/portfolio-website/static/js/runtime-main.5816c056.js"
+  }
+]);

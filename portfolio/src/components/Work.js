f2b163d502851f6212d2d8b7397f3cddf7a61b4a
Martyna Gorna <mart.gorna@gmail.com>
2020-09-21 12:59:34 +0100

    Background for the Work page

---
+++ portfolio/src/components/Work.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/Work.css";
+import { FaCubes } from "react-icons/fa";
+
+const Work = () => {
+  return (
+    <div className="workOuter">
+      <div className="cubesOuter">
+        <FaCubes className="cubes" />
+      </div>
+    </div>
+  );
+};
+
+export default Work;
